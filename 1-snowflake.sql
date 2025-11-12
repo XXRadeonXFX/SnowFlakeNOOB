845478fdbde128be3e8765b15c7df0cf434d4056
XXRadeonXFX <101211474+XXRadeonXFX@users.noreply.github.com>
2025-11-12 13:02:21 +0530

    Update 1-snowflake.sql

--- 1-snowflake.sql
+++ 1-snowflake.sql
@@ -27,24 +27,34 @@ t.$1:total::int AS  record_count
 ,t.$1:count::int AS  count
 
 --FROM record KEY
-,t.$1:records[1].last_update::text AS record_ts
+,t.$1:records[2].last_update::text AS record_ts
 
 -- Station Detail
-,t.$1:records[1].country::text AS country
-,t.$1:records[1].state::text AS state
-,t.$1:records[1].city::text AS city
-,t.$1:records[1].station::text AS station
-,t.$1:records[1].latitude::text AS latitude
-,t.$1:records[1].longitude::text AS longitude
+,t.$1:records[2].country::text AS country
+,t.$1:records[2].state::text AS state
+,t.$1:records[2].city::text AS city
+,t.$1:records[2].station::text AS station
+,t.$1:records[2].latitude::text AS latitude
+,t.$1:records[2].longitude::text AS longitude
 
 
 
 --Pollution Detail
-,t.$1:records[1].pollutant_id::text AS pollution_id
-,t.$1:records[1].pollutant_min::text AS pollution_min
-,t.$1:records[1].pollutant_max::text AS pollution_max
-,t.$1:records[1].pollutant_avg::text AS pollution_avg 
+,t.$1:records[2].pollutant_id::text AS pollution_id
+,t.$1:records[2].pollutant_min::text AS pollution_min
+,t.$1:records[2].pollutant_max::text AS pollution_max
+,t.$1:records[2].pollutant_avg::text AS pollution_avg 
 FROM @leetcode.public.stage_location ( file_format => my_json_ff ) t;
+
+
+
+SELECT
+  TRY_TO_TIMESTAMP( t.$1:records[0].last_update::text, 'dd-mm-yyyy hh24:mi:ss' ) AS index_records_ts,
+  t.$1:total::int AS record_count,
+  t.$1:version::text as json_version
+
+FROM @leetcode.public.stage_location
+( file_format => my_json_ff ) t;
 
 
 
