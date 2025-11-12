94e11e513303f9f8f45ceb192613522913663d75
XXRadeonXFX <101211474+XXRadeonXFX@users.noreply.github.com>
2025-11-12 14:35:01 +0530

    Update 3-stage-layer.sql

--- 3-stage-layer.sql
+++ 3-stage-layer.sql
@@ -42,5 +42,18 @@ FROM
 
 
 --level-3     
+SELECT
+    TRY_TO_TIMESTAMP(t.$1:records[0].last_update::text , 'dd-mm-yyyy hh24:mi:ss'  ) AS index_record_ts,
+    t.$1,
+    t.$1:total::int AS record_count,
+    t.$1:version::text AS json_version,
+    -- meta data information
+    metadata$filename AS _stg_file_name,
+    metadata$FILE_LAST_MODIFIED AS _stg_file_load_ts,
+    metadata$FILE_CONTENT_KEY AS _stg_file_md5,
+    CURRENT_TIMESTAMP() AS _copy_data_ts
     
+FROM 
+    @dev_db.stage_sch.raw_stg
+    (FILE_FORMAT => JSON_FILE_FORMAT) t;    
     
