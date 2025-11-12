25d81121892d95c7c232d204ef63ca961d05756d
XXRadeonXFX <101211474+XXRadeonXFX@users.noreply.github.com>
2025-11-12 17:39:24 +0530

    Update and rename 4-clear-layer.sql to 4-Clear-layer.sql

--- 4-Clear-layer.sql
+++ 4-Clear-layer.sql
@@ -68,6 +68,7 @@ where
     index_record_ts is not null; -- this will give all 24 records
 
 
+    
 
 -- de-duplication of the records + flattening it
 with air_quality_with_rank as (
@@ -114,51 +115,54 @@ unique_air_quality_data as (
 
 
 -- creating dynamic table
-create or replace dynamic table clean_aqi_dt
-    target_lag='downstream'
-    warehouse=transform_wh
-as
-  with air_quality_with_rank as (
-    select 
+--Tells Snowflake to refresh when upstream data changes (event-driven)
+
+-- TARGET_LAG= '5 MINUTES', '1 HOUR', '1 DAY' ,ON CHANGE (limited preview), DOWNSTREAM
+
+
+CREATE OR REPLACE DYNAMIC TABLE clean_aqi_dt
+    TARGET_LAG= 'DOWNSTREAM'
+    WAREHOUSE= Transform_wh
+AS 
+   WITH air_quality_with_rank AS (
+   SELECT 
         index_record_ts,
         json_data,
         record_count,
-        json_version,
         _stg_file_name,
         _stg_file_load_ts,
-        _stg_file_md5 ,
+        _stg_file_md5,
         _copy_data_ts,
-        row_number() over (partition by index_record_ts order by _stg_file_load_ts desc) as latest_file_rank
-    from dev_db.stage_sch.raw_aqi
-    where index_record_ts is not null
-),
-unique_air_quality_data as (
-    select 
-        * 
-    from 
-        air_quality_with_rank 
-    where latest_file_rank = 1
-)
-    select 
+        ROW_NUMBER() OVER( PARTITION BY index_record_ts ORDER BY _stg_file_load_ts DESC ) AS latest_file_rank
+   FROM dev_db.stage_sch.raw_aqi
+   WHERE index_record_ts IS NOT NULL
+   ) ,
+   unique_air_quality_data AS (
+   SELECT *
+   FROM 
+        air_quality_with_rank
+    WHERE latest_file_rank = 1
+)   
+    SELECT 
         index_record_ts,
-        hourly_rec.value:country::text as country,
-        hourly_rec.value:state::text as state,
-        hourly_rec.value:city::text as city,
-        hourly_rec.value:station::text as station,
-        hourly_rec.value:latitude::number(12,7) as latitude,
-        hourly_rec.value:longitude::number(12,7) as longitude,
-        hourly_rec.value:pollutant_id::text as pollutant_id,
-        hourly_rec.value:pollutant_max::text as pollutant_max,
-        hourly_rec.value:pollutant_min::text as pollutant_min,
-        hourly_rec.value:pollutant_avg::text as pollutant_avg,
+        hourly_rec.value:country::text AS country,
+        hourly_rec.value:state::text AS state,
+        hourly_rec.value:city::text AS city,
+        hourly_rec.value:station::text AS station,
+        hourly_rec.value:latitude::number(12,7) AS latitude,
+        hourly_rec.value:longitude::number(12,7) AS longitude,
+        hourly_rec.value:pollutant_id::text AS pollutant_id,
+        hourly_rec.value:pollutant_max::text AS pollutant_max,
+        hourly_rec.value:pollutant_min::text AS pullutant_min,
+        hourly_rec.value:pollutant_avg::text AS pollutant_avg,
 
         _stg_file_name,
         _stg_file_load_ts,
         _stg_file_md5,
         _copy_data_ts
-  from 
-    unique_air_quality_data ,
-    lateral flatten (input => json_data:records) hourly_rec;
+    FROM 
+        unique_air_quality_data ,
+        LATERAL FLATTEN( input => json_data:records ) hourly_rec; 
+        
 
-
-  select * from clean_aqi_dt limit 10;
+SELECT * FROM clean_aqi_dt limit 10;        
